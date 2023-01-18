a1d4b9f0429e4bd49252933f00f58cf58fbeba90
agordeev <gordeev.alex.v@gmail.com>
2023-01-18 00:27:12 +0000

    refactoring RocketImageCell with .. operator

--- SpaceX-Rockets/Modules/Rocket/View/Cell/RocketImageCell.swift
+++ SpaceX-Rockets/Modules/Rocket/View/Cell/RocketImageCell.swift
@@ -2,7 +2,7 @@
 //  RocketImageCell.swift
 //  SpaceX-Rockets
 //
-//  Created by Александр on 12.09.2022.
+//  Created by Aleksandr Gordeev on 12.09.2022.
 //
 
 import UIKit
@@ -10,62 +10,34 @@ import UIKit
 // MARK: RocketImageCell
 final class RocketImageCell: UICollectionViewCell {
     
-    private var imageView = UIImageView()
+    private lazy var roundedView = UIView() .. {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.backgroundColor = .appBlack
+        $0.layer.cornerRadius = 25
+        $0.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+    }
+    
+    private lazy var imageView = UIImageView() .. {
+        addSubview($0)
+        $0.addSubview(roundedView)
         
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setupUI()
-    }
-    
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        setupUI()
-    }
-    
-    func setImage(_ image: UIImage?) {
-        imageView.image = image
-    }
-}
-
-// MARK: Setup UI
-private extension RocketImageCell {
-    
-    func setupUI() {
-        backgroundColor = .clear
-                
-        setupImageView()
-        setupRoundedView()
-    }
-    
-    func setupImageView() {
-        addSubview(imageView)
-        clipsToBounds = true
-        
-        imageView.contentMode = .center
-        imageView.translatesAutoresizingMaskIntoConstraints = false
+        $0.contentMode = .center
+        $0.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            imageView.topAnchor.constraint(equalTo: topAnchor, constant: -30),
-            imageView.leftAnchor.constraint(equalTo: leftAnchor),
-            imageView.rightAnchor.constraint(equalTo: rightAnchor),
-            imageView.bottomAnchor.constraint(equalTo: bottomAnchor)
-        ])
-    }
-    
-    func setupRoundedView() {
-        let roundedView = UIView()
-        roundedView.translatesAutoresizingMaskIntoConstraints = false
-        roundedView.backgroundColor = .appBlack
-        roundedView.layer.cornerRadius = 25
-        roundedView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
-        
-        imageView.addSubview(roundedView)
-        
-        NSLayoutConstraint.activate([
+            $0.topAnchor.constraint(equalTo: topAnchor, constant: -30),
+            $0.leftAnchor.constraint(equalTo: leftAnchor),
+            $0.rightAnchor.constraint(equalTo: rightAnchor),
+            $0.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
             roundedView.heightAnchor.constraint(equalToConstant: 65),
-            roundedView.leftAnchor.constraint(equalTo: imageView.leftAnchor),
-            roundedView.rightAnchor.constraint(equalTo: imageView.rightAnchor),
-            roundedView.bottomAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 40)
+            roundedView.leftAnchor.constraint(equalTo: $0.leftAnchor),
+            roundedView.rightAnchor.constraint(equalTo: $0.rightAnchor),
+            roundedView.bottomAnchor.constraint(equalTo: $0.bottomAnchor, constant: 40)
         ])
+    }
+    
+    public func setImage(_ image: UIImage?) {
+        imageView.image = image
     }
 }

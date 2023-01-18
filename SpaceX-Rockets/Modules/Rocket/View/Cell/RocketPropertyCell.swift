0c0bbca76825febd19b61c8c66093dca150b851b
agordeev <gordeev.alex.v@gmail.com>
2023-01-18 00:27:28 +0000

    refactoring RocketPropertyCell with .. operator

--- SpaceX-Rockets/Modules/Rocket/View/Cell/RocketPropertyCell.swift
+++ SpaceX-Rockets/Modules/Rocket/View/Cell/RocketPropertyCell.swift
@@ -2,7 +2,7 @@
 //  RocketPropertyCell.swift
 //  SpaceX-Rockets
 //
-//  Created by Александр on 18.08.2022.
+//  Created by Aleksandr Gordeev on 18.08.2022.
 //
 
 import UIKit
@@ -10,68 +10,41 @@ import UIKit
 // MARK: RocketPropertyCell
 final class RocketPropertyCell: UICollectionViewCell {
             
-    private let valueLabel = UILabel()
-    private let measureLabel = UILabel()
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setupUI()
-    }
-    
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        setupUI()
-    }
-    
-    func configure(valueText: String, measureText: String) {
-        valueLabel.text = valueText
-        measureLabel.text = measureText
-    }
-}
-
-// MARK: Setup UI Methods
-extension RocketPropertyCell {
-    
-    private func setupUI() {
-        setupCell()
-        setupValueLabel()
-        setupMeasureLabel()
-    }
-    
-    private func setupCell() {
-        backgroundColor = .appDarkGray
-        layer.cornerRadius = 25
-    }
-    
-    private func setupValueLabel() {
-        addSubview(valueLabel)
+    private lazy var valueLabel = UILabel() .. {
+        addSubview($0)
         
-        valueLabel.font = UIFont.labGrotesqueFontRegular(ofSize: 16)
-        valueLabel.textColor = .appWhite
-        valueLabel.translatesAutoresizingMaskIntoConstraints = false
-        valueLabel.textAlignment = .center
+        $0.font = UIFont.labGrotesqueFontRegular(ofSize: 14)
+        $0.textColor = .appWhite
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.textAlignment = .center
+        $0.adjustsFontSizeToFitWidth = true
                 
         NSLayoutConstraint.activate([
-            valueLabel.leftAnchor.constraint(equalTo: leftAnchor, constant: 5),
-            valueLabel.rightAnchor.constraint(equalTo: rightAnchor, constant: -5),
-            valueLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
-            valueLabel.centerYAnchor.constraint(equalTo: centerYAnchor, constant: -10)
+            $0.leftAnchor.constraint(equalTo: leftAnchor, constant: 5),
+            $0.rightAnchor.constraint(equalTo: rightAnchor, constant: -5),
+            $0.centerXAnchor.constraint(equalTo: centerXAnchor),
+            $0.centerYAnchor.constraint(equalTo: centerYAnchor, constant: -10)
         ])
     }
     
-    private func setupMeasureLabel() {
-        addSubview(measureLabel)
+    private lazy var measureLabel = UILabel() .. {
+        addSubview($0)
         
-        measureLabel.font = UIFont.labGrotesqueFontRegular(ofSize: 14)
-        measureLabel.textColor = .appGray
-        measureLabel.translatesAutoresizingMaskIntoConstraints = false
-        measureLabel.textAlignment = .center
+        $0.font = UIFont.labGrotesqueFontRegular(ofSize: 13)
+        $0.textColor = .appGray
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.textAlignment = .center
         
         NSLayoutConstraint.activate([
-            measureLabel.widthAnchor.constraint(equalTo: widthAnchor),
-            measureLabel.heightAnchor.constraint(equalToConstant: 24),
-            measureLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
-            measureLabel.centerYAnchor.constraint(equalTo: centerYAnchor, constant: 12)
+            $0.widthAnchor.constraint(equalTo: widthAnchor),
+            $0.heightAnchor.constraint(equalToConstant: 24),
+            $0.centerXAnchor.constraint(equalTo: centerXAnchor),
+            $0.centerYAnchor.constraint(equalTo: centerYAnchor, constant: 12)
         ])
+    }
+    
+    public func configure(valueText: String, measureText: String) {
+        valueLabel.text = valueText
+        measureLabel.text = measureText
     }
 }

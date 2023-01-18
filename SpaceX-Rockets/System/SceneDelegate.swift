2e93e3388151dc7aa69610447651d10729172338
agordeev <gordeev.alex.v@gmail.com>
2023-01-18 00:25:04 +0000

    clean AppDelegate and SceneDelegate

--- SpaceX-Rockets/System/SceneDelegate.swift
+++ SpaceX-Rockets/System/SceneDelegate.swift
@@ -2,44 +2,39 @@
 //  SceneDelegate.swift
 //  SpaceX-Rockets
 //
-//  Created by Александр on 17.08.2022.
+//  Created by Aleksandr Gordeev on 17.08.2022.
 //
 
 import UIKit
 
 // MARK: SceneDelegate
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
     // MARK: - navigationController
-    lazy var navigationController: UINavigationController = {
-        let navigationController = UINavigationController()
-        
-        navigationController.modalPresentationStyle = .fullScreen
-        
-        navigationController.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.appWhite]
-        navigationController.navigationBar.setBackgroundImage(UIImage(), for: .default)
-        navigationController.navigationBar.shadowImage = UIImage()
-        navigationController.navigationBar.tintColor = .appWhite
-        
-        return navigationController
-    }()
+    private lazy var navigationController = UINavigationController() .. {
+        $0.modalPresentationStyle = .fullScreen
+        $0.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.appWhite]
+        $0.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        $0.navigationBar.shadowImage = UIImage()
+        $0.navigationBar.tintColor = .appWhite
+    }
 
     // MARK: - scene
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession,
                options connectionOptions: UIScene.ConnectionOptions) {
          
         guard let windowsScene = (scene as? UIWindowScene) else { return }
-        
         let moduleBuilder = ModuleBuilder()
         
         let router = Router(navigationController: navigationController, assemblyBuilder: moduleBuilder)
         router.initialViewContoller()
         
-        window = UIWindow(frame: windowsScene.coordinateSpace.bounds)
-        window?.windowScene = windowsScene
-        window?.rootViewController = navigationController
-        window?.makeKeyAndVisible()
+        window = UIWindow(frame: windowsScene.coordinateSpace.bounds) .. {
+            $0.windowScene = windowsScene
+            $0.rootViewController = navigationController
+            $0.makeKeyAndVisible()
+        }
     }
 }

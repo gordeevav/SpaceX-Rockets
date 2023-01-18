adf4e97329bd771fbf4bf11971cff4af1dca31b1
agordeev <gordeev.alex.v@gmail.com>
2023-01-18 00:25:34 +0000

    refactoring ErrorViewController with .. operator

--- SpaceX-Rockets/Modules/Error/ErrorViewController.swift
+++ SpaceX-Rockets/Modules/Error/ErrorViewController.swift
@@ -2,7 +2,7 @@
 //  ErrorViewController.swift
 //  SpaceX-Rockets
 //
-//  Created by Александр on 31.08.2022.
+//  Created by Aleksandr Gordeev on 31.08.2022.
 //
 
 import UIKit
@@ -10,65 +10,49 @@ import UIKit
 // MARK: ErrorViewController
 final class ErrorViewController: UIViewController, ErrorViewProtocol {
     
-    private let label = UILabel()
-    private let closeButton = UIButton()
-    
     var presenter: ErrorPresenterProtocol?
+    
+    private lazy var label = UILabel() .. {
+        view.addSubview($0)
+        
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.numberOfLines = 5
+        $0.textAlignment = .center
+        $0.textColor = .appWhite
+        
+        let layoutMargins = view.layoutMarginsGuide
+        $0.widthAnchor.constraint(equalTo: layoutMargins.widthAnchor).isActive = true
+        $0.centerXAnchor.constraint(equalTo: layoutMargins.centerXAnchor).isActive = true
+        $0.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        $0.centerYAnchor.constraint(equalTo: layoutMargins.centerYAnchor).isActive = true
+    }
+    
+    private lazy var closeButton = UIButton() .. {
+        view.addSubview($0)
+        
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.setTitle(NSLocalizedString("Error.Close", comment: ""), for: .normal)
+        $0.addTarget(self, action: #selector(closeButtonDidPress), for: .touchDown)
+        
+        let layoutMargins = view.layoutMarginsGuide
+        $0.widthAnchor.constraint(equalTo: layoutMargins.widthAnchor).isActive = true
+        $0.centerXAnchor.constraint(equalTo: layoutMargins.centerXAnchor).isActive = true
+        $0.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        $0.bottomAnchor.constraint(equalTo: layoutMargins.bottomAnchor).isActive = true
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupUI()
+        
+        title = NSLocalizedString("Error.Critical Error", comment: "")
     }
 
-    func showError(text: String) {
+    public func showError(text: String) {
         label.text = text
     }
-}
-
-// MARK: Button Targets
-extension ErrorViewController {
     
-    @objc private func closeButtonDidPress() {
+    @objc
+    private func closeButtonDidPress() {
         presenter?.closeErrorView()
-    }
-}
-
-// MARK: Setup UI Methods
-private extension ErrorViewController {
-    
-    func setupUI() {
-        title = NSLocalizedString("Error.Critical Error", comment: "")
-        
-        setupLabel()
-        setupCloseButton()
-    }
-    
-    func setupLabel() {
-        view.addSubview(label)
-        
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.numberOfLines = 5
-        label.textAlignment = .center
-        label.textColor = .appWhite
-        
-        let layoutMargins = view.layoutMarginsGuide
-        label.widthAnchor.constraint(equalTo: layoutMargins.widthAnchor).isActive = true
-        label.centerXAnchor.constraint(equalTo: layoutMargins.centerXAnchor).isActive = true
-        label.heightAnchor.constraint(equalToConstant: 100).isActive = true
-        label.centerYAnchor.constraint(equalTo: layoutMargins.centerYAnchor).isActive = true
-    }
-    
-    func setupCloseButton() {
-        view.addSubview(closeButton)
-        
-        closeButton.translatesAutoresizingMaskIntoConstraints = false
-        closeButton.setTitle(NSLocalizedString("Error.Close", comment: ""), for: .normal)
-        closeButton.addTarget(self, action: #selector(closeButtonDidPress), for: .touchDown)
-        
-        let layoutMargins = view.layoutMarginsGuide
-        closeButton.widthAnchor.constraint(equalTo: layoutMargins.widthAnchor).isActive = true
-        closeButton.centerXAnchor.constraint(equalTo: layoutMargins.centerXAnchor).isActive = true
-        closeButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
-        closeButton.bottomAnchor.constraint(equalTo: layoutMargins.bottomAnchor).isActive = true
     }
 }
